00acd20e13e58f66b4ec5b37ef2536f366b47dbe
VentGrey <omarpurataf@gmail.com>
2023-10-13 14:11:13 -0600

    Write dbus notification function

--- notify/notify.go
+++ notify/notify.go
@@ -4,6 +4,12 @@ as well as data structures and constants.
 */
 package notify
 
+import (
+	"log"
+
+	"github.com/godbus/dbus/v5"
+)
+
 // NotificationStatus represents the current state of battery notifications.
 // It determines whether a particular notification has been shown to the user.
 type NotificationStatus int
@@ -18,3 +24,21 @@ const (
 	// VeryLowBatteryNotified indicates that a critical battery notification has been shown.
 	VeryLowBatteryNotified
 )
+
+// SendNotification connects to the current session bus and tries to trigger
+// a new notification via the freedesktop notifications interface.
+func SendNotification(title, message, icon string) {
+	conn, err := dbus.SessionBus()
+
+	if err != nil {
+		log.Println("Failed to connect to session bus: ", err)
+		return
+	}
+
+	obj := conn.Object("org.freedesktop.Notifications", "/org/freedesktop/Notifications")
+	call := obj.Call("org.freedesktop.Notifications.Notify", 0, "", uint32(0),
+		icon, title, message, []string{}, map[string]dbus.Variant{}, int32(5000))
+	if call.Err != nil {
+		log.Println("Failed to send notification:", call.Err)
+	}
+}
